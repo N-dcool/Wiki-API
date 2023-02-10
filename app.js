3fa4a7e92405e4f2c9fb45877e2ea29fcc845811
NARESH CHOUDHARY <nareshnc82643@gmail.com>
2023-02-10 22:45:01 +0530

    added variable env port in app.js

--- app.js
+++ app.js
@@ -119,6 +119,8 @@ app.route("/articles/:articleTitle")
     );
 });
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
     console.log('Server is running on port 3000');
 });
